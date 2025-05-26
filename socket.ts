d6e46a54d50bdba345e025fab4ba558119b0f0c8
yemishi <daviddperera10@gmail.com>
2025-05-26 18:20:51 +0100

    migrate project from Next.js to React.js 19 with Vite setup

--- socket.ts
+++
@@ -1,9 +0,0 @@
-import { io } from 'socket.io-client';
-
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3001';
-
-const socket = io(URL);
-socket.emit("getOnlineUsers")
-
-export default socket
-
