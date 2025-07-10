6b531b57a76e77f6dfb6265e4232ad11f1745e2f
yemishi <daviddperera10@gmail.com>
2025-07-10 19:13:44 +0100

    using chatroom component(temp) and connect to server

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
-import "./index.css"
+import "./index.css";
+import ChatRoom from "./components/ChatRoom";
+import { useState } from "react";
+import { connectSocket } from "./lib/socket";
+
 function App() {
+  const [userId, setUserId] = useState("");
+  const [room, setRoom] = useState("");
+  const [isChat, setIsChat] = useState(false);
+
   return (
-    <div className="bg-black p-2">
-      <h1>App page raah</h1>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="bg-black  text-white p-4 flex flex-col gap-3">
+        {isChat ? (
+          <>
+            <ChatRoom userId={userId} roomId={room} />
+          </>
+        ) : (
+          <>
+            <input
+              placeholder="userId"
+              onChange={(e) => setUserId(e.target.value)}
+              value={userId}
+              className="flex-1 p-2 rounded bg-zinc-800 border border-zinc-700 text-white"
+            />
+            <input
+              placeholder="room"
+              onChange={(e) => setRoom(e.target.value)}
+              value={room}
+              className="flex-1 p-2 rounded bg-zinc-800 border border-zinc-700 text-white"
+            />
+            <button
+              onClick={() => {
+                {
+                  connectSocket(userId);
+                  setIsChat(true);
+                }
+              }}
+              className="px-4 py-2 bg-orange-600 hover:bg-orange-700 rounded text-white"
+            >
+              Get on!!
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
