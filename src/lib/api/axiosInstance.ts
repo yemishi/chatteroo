bb12e86b1e06a19094bc3f2505e048b53b51e87b
yemishi <daviddperera10@gmail.com>
2025-07-14 20:32:00 +0100

    add axios instance

--- src/lib/api/axiosInstance.ts
+++ src/lib/api/axiosInstance.ts
@@ -4,6 +4,7 @@ const API_URL = import.meta.env.VITE_SERVER_URL;
 
 export const axiosInstance = axios.create({
   baseURL: `${API_URL}/api`,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
