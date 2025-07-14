48915d1fe182de3a4bdcc4afc09cd83bcabddde5
yemishi <daviddperera10@gmail.com>
2025-07-14 20:31:30 +0100

    testing userContext and sign/signout actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import "./index.css";
 import { useAuth } from "./context/AuthContext";
+import { useAuthActions } from "./hooks/useAuthActions";
 
 export default function App() {
-  const { user, isLoading } = useAuth();
-
+  const { user, isLoading, error } = useAuth();
+  const { guestRegister, guestLogin, signout } = useAuthActions();
   if (isLoading) return <div>loading....</div>;
-  if (!user) return <div>no user</div>;
+  if (!user || error)
+    return (
+      <div>
+        <button onClick={() => guestRegister.mutate()}>Register guest</button>
+        <button onClick={() => guestLogin.mutate({ guestId: "952d3b5b-65ae-43da-a8dd-ba324f987b79" })}>
+          Login guest
+        </button>
+      </div>
+    );
 
-  return <p>Welcome {user.username}!</p>;
+  return (
+    <div>
+      <p onClick={() => console.log(user)}>Welcome {user.username}!</p>
+      <button
+        onClick={() => {
+          signout.mutate();
+        }}
+      >
+        signOut
+      </button>
+    </div>
+  );
 }
