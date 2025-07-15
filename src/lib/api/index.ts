e2c77078f9f62424103e814a040cd6df189d38a2
yemishi <daviddperera10@gmail.com>
2025-07-15 20:30:55 +0100

    importing and exporting friends and contections api calls

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -2,3 +2,9 @@ export * from "./auth/login";
 export * from "./auth/signout";
 export * from "./auth/getUser";
 export * from "./auth/register";
+
+export * from "./friends/friendRequest";
+export * from "./friends/friends";
+
+export * from "./connections/chat";
+export * from "./connections/message";
