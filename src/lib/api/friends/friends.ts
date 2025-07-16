1a10a3d4abfff0e6e5e2a4fa622365116247783d
yemishi <daviddperera10@gmail.com>
2025-07-16 20:20:38 +0100

    api routes for user friends methods

--- src/lib/api/friends/friends.ts
+++ src/lib/api/friends/friends.ts
@@ -1,14 +1,5 @@
 import { axiosInstance } from "../axiosInstance";
 
-export const getFriends = async (userId: string, take?: number, page?: number) => {
-  try {
-    const response = await axiosInstance.get(`/friends/${userId}?take=${take}&page=${page}`);
-    return response.data;
-  } catch (error: any) {
-    throw new Error(error.response.data.message);
-  }
-};
-
 export const removeFriend = async (userId: string) => {
   try {
     const response = await axiosInstance.patch(`/friends/${userId}`);
