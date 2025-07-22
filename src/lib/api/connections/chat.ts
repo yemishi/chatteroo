33450ddd63c1683e1fafd4d2360463a4fcb4f4c6
yemishi <daviddperera10@gmail.com>
2025-07-22 15:45:48 +0100

    add getChats fetch

--- src/lib/api/connections/chat.ts
+++ src/lib/api/connections/chat.ts
@@ -1,9 +1,19 @@
+import type { Chat } from "@/types";
 import { axiosInstance } from "../axiosInstance";
 
 export const getChatInfo = async (chatId: string) => {
   try {
     const response = await axiosInstance.get(`/chat/${chatId}`);
     return response.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const getChats = async () => {
+  try {
+    const response = await axiosInstance.get(`/chat`);
+    return response.data as Chat[];
   } catch (error: any) {
     throw new Error(error.response.data.message);
   }
