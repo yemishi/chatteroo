9edeb1003b1a755ab8f50060a4c42c0a4f65b287
yemishi <daviddperera10@gmail.com>
2025-07-28 19:28:24 +0100

    getting the user theme preference

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
+import "./styles/main.scss";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
+
+const darkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const root = document.documentElement;
+if (darkTheme) {
+  root.setAttribute("data-theme", "dark");
+} else {
+  root.setAttribute("data-theme", "light");
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
