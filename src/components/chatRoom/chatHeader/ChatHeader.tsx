b5062730138939bf1d0f0dceb1e5ba7ae5461367
yemishi <daviddperera10@gmail.com>
2025-07-29 21:33:24 +0100

    extracted component from chatRoom for clarify

---
+++ src/components/chatRoom/chatHeader/ChatHeader.tsx
@@ -0,0 +1,27 @@
+
+import "./styles.scss";
+import arrowLeft from "@/assets/icons/arrow-left.svg";
+import menuIcon from "@/assets/icons/menu.svg";
+import type { ChatMember } from "@/types";
+
+type Props = {
+  onClose: () => void;
+  userHighlight: ChatMember;
+};
+
+export default function ChatHeader({ onClose, userHighlight: { picture, username } }: Props) {
+  return (
+    <header className="chat-header">
+      <button onClick={onClose}>
+        <img className="icon" src={arrowLeft} alt="arrow left" />
+      </button>
+      <div className="chat-user-info">
+        <img src={picture} alt={`${username}'s profile picture`} />
+        <p>{username}</p>
+      </div>
+      <button>
+        <img className="icon" src={menuIcon} alt="menu icon" />
+      </button>
+    </header>
+  );
+}
