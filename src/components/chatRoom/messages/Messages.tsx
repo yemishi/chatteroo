7e3d0363c499f7bd5003aca8b2500582c6f99dab
yemishi <daviddperera10@gmail.com>
2025-07-29 21:33:37 +0100

    extracted component from chatRoom for clarify

---
+++ src/components/chatRoom/messages/Messages.tsx
@@ -0,0 +1,77 @@
+import "./styles.scss";
+import { formatTime } from "@/helpers";
+import type { Message } from "@/hooks/useChat";
+import type { Message as MessageData, User, ChatMember } from "@/types";
+import { Fragment } from "react/jsx-runtime";
+
+type Props = {
+  messages: (Message | MessageData)[];
+  currUser: User;
+  members: ChatMember[];
+};
+
+export default function Messages({ messages, currUser, members }: Props) {
+  const isDiffDay = (d1: Date, d2: Date) =>
+    d1.getFullYear() !== d2.getFullYear() || d1.getMonth() !== d2.getMonth() || d1.getDate() !== d2.getDate();
+
+  const isSameDay = (d1: Date, d2: Date) =>
+    d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate();
+
+  const isYesterday = (d1: Date, d2: Date) => {
+    const yesterday = new Date(d2);
+    yesterday.setDate(d2.getDate() - 1);
+    return isSameDay(d1, yesterday);
+  };
+
+  return (
+    <div className="messages">
+      {messages.map((msg, i) => {
+        const user = members.find((u) => u.id === msg.senderId)!;
+        const isMe = msg.senderId === currUser?.id;
+        const nextMessage = messages[i + 1];
+        const isSameUser = nextMessage?.senderId === msg.senderId;
+        const nextTime = nextMessage ? new Date(nextMessage?.timestamp) : null;
+        const prevTime = messages[i - 1] ? new Date(messages[i - 1].timestamp) : null;
+        const thisTime = new Date(msg.timestamp);
+        const today = new Date().getDate();
+
+        const isNewDay = !prevTime || isDiffDay(prevTime, thisTime);
+
+        const isCloseInTime = nextTime && Math.abs(new Date(msg.timestamp).getTime() - nextTime.getTime()) < 60 * 1000;
+
+        return (
+          <Fragment key={`${msg.timestamp}_${i}`}>
+            {isNewDay && (
+              <div className="day-divider">
+                {isSameDay(thisTime, new Date())
+                  ? "Today"
+                  : isYesterday(thisTime, new Date())
+                  ? "Yesterday"
+                  : thisTime.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+              </div>
+            )}
+            <div className={`message-container ${isMe ? "sent" : "received"}`}>
+              {!isMe && (
+                <img
+                  src={user.picture}
+                  alt={`${user.username} picture`}
+                  className={`${!nextMessage || !isSameUser ? "show" : ""} message-user-pic`}
+                />
+              )}
+              <div className="message-content">
+                <p>{msg.content}</p>
+                <span className={`message-time ${isCloseInTime && isSameUser ? "hide" : ""}`}>
+                  {formatTime(msg.timestamp.toString())}
+                </span>
+              </div>
+            </div>
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+}
