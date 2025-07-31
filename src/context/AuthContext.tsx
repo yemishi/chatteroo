6394db511e5203cc7863c8b265d967506b14ac51
yemishi <daviddperera10@gmail.com>
2025-07-31 20:46:02 +0100

    add updateUser method to AuthContext for manual user state updates

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,17 +1,25 @@
-import { createContext, useContext, useEffect, type ReactNode } from "react";
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getUser as queryFn } from "@/lib/api";
 import { connectSocket, getSocket } from "@/lib/socket";
 import type { User } from "@/types";
 
-const AuthContext = createContext<{ isLoading: boolean; user: User | null; refetch: () => void; error: Error | null }>({
+const AuthContext = createContext<{
+  isLoading: boolean;
+  user: User | null;
+  refetch: () => void;
+  error: Error | null;
+  updateUser: (user: User | null) => void;
+}>({
   user: null,
   isLoading: true,
   refetch: () => {},
+  updateUser: () => {},
   error: null,
 });
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
   const { data, isLoading, isRefetching, isFetching, refetch, error } = useQuery({
     queryKey: ["currentUser"],
     queryFn,
@@ -19,11 +27,12 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
     if (data && !getSocket()) {
       connectSocket(data.id);
+      setUser(data);
     }
   }, [data]);
-
+  const updateUser = (user: User | null) => setUser(user);
   return (
-    <AuthContext value={{ user: data, isLoading: isLoading || isRefetching || isFetching, refetch, error }}>
+    <AuthContext value={{ user, isLoading: isLoading || isRefetching || isFetching, refetch, error, updateUser }}>
       {children}
     </AuthContext>
   );
