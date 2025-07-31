3f7c4565d8b689693add891f1cbdd6c73f7f40e2
yemishi <daviddperera10@gmail.com>
2025-07-31 20:36:29 +0100

    show online status in ChatHeader using getOnlineUsers

--- src/components/chatRoom/chatHeader/ChatHeader.tsx
+++ src/components/chatRoom/chatHeader/ChatHeader.tsx
@@ -1,7 +1,7 @@
-
 import "./styles.scss";
 import arrowLeft from "@/assets/icons/arrow-left.svg";
 import menuIcon from "@/assets/icons/menu.svg";
+import getOnlineUsers from "@/lib/getOnlineUsers";
 import type { ChatMember } from "@/types";
 
 type Props = {
@@ -9,14 +9,19 @@ type Props = {
   userHighlight: ChatMember;
 };
 
-export default function ChatHeader({ onClose, userHighlight: { picture, username } }: Props) {
+export default function ChatHeader({ onClose, userHighlight: { picture, username, id } }: Props) {
+  const onlineUsers = getOnlineUsers();
   return (
     <header className="chat-header">
       <button onClick={onClose}>
         <img className="chat-icon" src={arrowLeft} alt="arrow left" />
       </button>
       <div className="chat-user-info">
-        <img src={picture} alt={`${username}'s profile picture`} />
+        <img
+          src={picture}
+          className={onlineUsers.includes(id) ? "online" : "offline"}
+          alt={`${username}'s profile picture`}
+        />
         <p>{username}</p>
       </div>
       <button>
