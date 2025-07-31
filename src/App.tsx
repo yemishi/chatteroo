f53301ff9735f744c6fe475fc14df0bbe768d382
yemishi <daviddperera10@gmail.com>
2025-07-31 20:33:58 +0100

    add dynamic HomeHeader with search input and time based greeting

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import { authActions } from "@/lib/actions";
 import ChatList from "./components/chatList/ChatList";
 import type { Chat as ChatType } from "./types";
 import Chat from "./components/chatRoom/ChatRoom";
-import getOnlineUsers from "./lib/getOnlineUsers";
-import { getSocket } from "./lib/socket";
+import HomeHeader from "./components/homeHeader/Header";
 
 export default function App() {
   const { user, isLoading, error } = useAuth();
-  const { guestRegister, guestLogin, signout } = authActions();
-
+  const { guestRegister, guestLogin } = authActions();
+  const [searchChat, setSearchChat] = useState("");
   const [scrollPositions, setScrollPositions] = useState<Record<string, number>>({});
   const [guestId, setGuestId] = useState("");
+
+  const handleSearchChat = (search: string) => setSearchChat(search.replace("  ", " "));
 
   const [chatInfo, setChatInfo] = useState<ChatType | null>(null);
   const handleGuestLogin = () => {
@@ -23,12 +24,12 @@ export default function App() {
     guestRegister.mutate();
   };
 
-  const onlineUsers = getOnlineUsers();
   if (isLoading) return <div>Loading...</div>;
 
-  if (!user || error) {
+  if (!user && error) {
     return (
       <div className="p-4 space-y-3">
+        <button onClick={() => console.log(user)}>Check user</button>
         <button onClick={handleGuestRegister}>Register as Guest</button>
         <input type="text" value={guestId} placeholder="Enter guest ID" onChange={(e) => setGuestId(e.target.value)} />
         <button onClick={handleGuestLogin}>Login as Guest</button>
@@ -37,16 +38,9 @@ export default function App() {
   }
   return (
     <main>
-      <button
-        onClick={() => {
-          signout.mutate();
-          const s = getSocket()!;
-          s.disconnect()
-        }}
-      >
-        get off
-      </button>
-      <ChatList setChat={setChatInfo} onlineUsers={onlineUsers} />
+      <HomeHeader onChangeSearch={handleSearchChat} search={searchChat} user={user!} />
+      <ChatList setChat={setChatInfo} searchChat={searchChat} />
+
       {chatInfo && (
         <Chat
           scrollPositions={scrollPositions}
