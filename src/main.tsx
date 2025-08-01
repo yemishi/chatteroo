ad7d2a343a13264159a2745e0a02c9cf09e858d8
yemishi <daviddperera10@gmail.com>
2025-08-01 16:22:58 +0100

    add tasntack react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import "./styles/main.scss";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { router } from "./router/index.ts";
+import { RouterProvider } from "@tanstack/react-router";
 
 const queryClient = new QueryClient();
 
@@ -20,7 +22,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <App />
+        <main>
+          <RouterProvider router={router} />
+        </main>
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
