d74f6d0cd47d572997d084d423816d76dfaa50f6
yemishi <daviddperera10@gmail.com>
2025-08-03 16:42:57 +0100

    add response type for searchUser action

--- src/lib/actions/handleSearch/handleSearchUser.ts
+++ src/lib/actions/handleSearch/handleSearchUser.ts
@@ -1,5 +1,6 @@
 import useScrollQuery from "@/hooks/useScrollQuery";
+import type { SearchUser } from "@/types/searchType";
 
 export const searchUser = (q?: string) => {
-  return useScrollQuery({ url: `/search/user?q=${q}`, queryKey: ["search-users", q || ""] });
+  return useScrollQuery<SearchUser>({ url: `/search/user?q=${q}`, queryKey: ["search-users", q || ""] });
 };
