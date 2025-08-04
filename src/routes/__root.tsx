da90c1a81f0b489a19cd86baa11ad9ee0a419fd5
yemishi <daviddperera10@gmail.com>
2025-08-04 21:25:51 +0100

    upgrade to createRootRouterWithContext with user context and loading state

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,13 @@
-import { createRootRoute } from "@tanstack/react-router";
+import type { User } from "@/types";
+import { createRootRouteWithContext } from "@tanstack/react-router";
 import { Outlet } from "@tanstack/react-router";
 
-export const rootRoute = createRootRoute({
+export const rootRoute = createRootRouteWithContext<{ user: User | null }>()({
   component: () => <Outlet />,
+  pendingComponent: () => <div>Loading...</div>,
+  validateSearch: (search: { redirect?: string }) => {
+    return {
+      redirect: search.redirect ?? undefined,
+    };
+  },
 });
