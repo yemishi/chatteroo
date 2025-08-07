eda02ee250cff895bfdd79ed110dba0058e64fa4
yemishi <daviddperera10@gmail.com>
2025-08-07 13:06:32 +0100

    add persistent NavBar and improve search validation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,22 @@
+import NavBar from "@/components/navBar/NavBar";
 import type { User } from "@/types";
 import { createRootRouteWithContext } from "@tanstack/react-router";
 import { Outlet } from "@tanstack/react-router";
 
 export const rootRoute = createRootRouteWithContext<{ user: User | null }>()({
-  component: () => <Outlet />,
+  component: () => (
+    <>
+      <Outlet />
+      <NavBar />
+    </>
+  ),
   pendingComponent: () => <div>Loading...</div>,
-  validateSearch: (search: { redirect?: string }) => {
-    return {
-      redirect: search.redirect ?? undefined,
-    };
+  validateSearch: (search?: { redirect?: string }) => {
+    if (search?.redirect) {
+      return {
+        redirect: search?.redirect ?? undefined,
+      };
+    }
+    return search;
   },
 });
