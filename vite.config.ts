78a0ce3c0f4fd4b919076b768b83461df566d886
yemishi <daviddperera10@gmail.com>
2025-08-07 13:02:26 +0100

    add svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import path from "path";
-
+import svgr from "vite-plugin-svgr";
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  plugins: [svgr()],
 });
