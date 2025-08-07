c568245c2ffdfbe7d2e315360c8b169f71f0cb40
yemishi <daviddperera10@gmail.com>
2025-08-07 13:05:36 +0100

    declaring default context value

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "@tanstack/react-router";
 import routeTree from "./routeTree";
 
-export const router = new Router({ routeTree });
+export const router = new Router({ routeTree, context: { user: null } });
 
 declare module "@tanstack/react-router" {
   interface Register {
