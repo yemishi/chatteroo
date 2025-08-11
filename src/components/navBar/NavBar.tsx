067d4b9479babd73b854ad804ad1da2c62ed0f88
yemishi <daviddperera10@gmail.com>
2025-08-11 15:26:10 +0100

    add 'Requests' link to NavBar and update profile icon classes

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -3,6 +3,7 @@ import { Link, useRouterState } from "@tanstack/react-router";
 import HomeIcon from "./assets/icons/chat.svg?react";
 import UserIcon from "./assets/icons/profile.svg?react";
 import SearchIcon from "@/assets/icons/search.svg?react";
+import FriendRequestIcon from "@/assets/icons/user-add.svg?react";
 
 export default function NavBar() {
   const pathname = useRouterState({
@@ -28,14 +29,23 @@ export default function NavBar() {
         <SearchIcon className={`nav-bar__icon ${isActive("/search") ? "nav-bar__icon--active" : ""}`} />
         <p>Search</p>
       </Link>
-
+      <Link
+        to="/requests"
+        className={`nav-bar__button ${isActive("/requests") ? "nav-bar__button--active" : ""}`}
+        aria-label="requests"
+      >
+        <FriendRequestIcon
+          className={`nav-bar__icon ${isActive("/requests") ? "nav-bar__icon--active" : ""} nav-bar__icon--request `}
+        />
+        <p>Request</p>
+      </Link>
       <Link
         to="/"
-        className={`nav-bar__button ${isActive("/profile") ? "nav-bar__button--active" : ""}`}
+        className={`nav-bar__button ${isActive("/profile") ? "nav-bar__button--active" : ""} `}
         aria-label="profile"
       >
         <UserIcon
-          className={`nav-bar__icon ${isActive("/profile") ? "nav-bar__icon--active" : ""}  nav-bar__icon--profile`}
+          className={`nav-bar__icon ${isActive("/profile") ? "nav-bar__icon--active" : ""}  nav-bar__icon--user`}
         />
         <p>Profile</p>
       </Link>
