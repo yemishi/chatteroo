80b5bc8310d7e850d44f01cf115841f981d2b9dc
yemishi <daviddperera10@gmail.com>
2025-08-16 12:32:30 +0100

    revamp Signin page with user/guest login forms

--- src/pages/login/signin/Signin.tsx
+++ src/pages/login/signin/Signin.tsx
@@ -1,26 +1,36 @@
-import { authActions } from "@/lib/actions";
-import { useRouter } from "@tanstack/react-router";
+import "../styles.scss";
+import { Link, useNavigate, useRouter } from "@tanstack/react-router";
 
+import UserLoginForm from "./userLoginForm/UserLoginForm";
 import { useState } from "react";
+import GuestLoginForm from "./guestLoginForm/GuestLoginForm";
 
 export default function Signin() {
-  const { guestRegister, guestLogin } = authActions();
-  const [guestId, setGuestId] = useState("");
   const { user } = useRouter().options.context;
-  const handleGuestLogin = () => {
-    guestLogin.mutate({ guestId });
-  };
+  const navigate = useNavigate();
+  if (user) {
+    navigate({ to: "/account" });
+    return;
+  }
 
-  const handleGuestRegister = () => {
-    guestRegister.mutate();
-  };
+  const [isGuestLogin, setIsGuestLogin] = useState(false);
 
   return (
-    <div className="p-4 space-y-3">
-      <button onClick={() => console.log(user)}>Check user</button>
-      <button onClick={handleGuestRegister}>Register as Guest</button>
-      <input type="text" value={guestId} placeholder="Enter guest ID" onChange={(e) => setGuestId(e.target.value)} />
-      <button onClick={handleGuestLogin}>Login as Guest</button>
+    <div className="auth-page">
+      <div className="auth-page__form-container">
+        <h1>Welcome!</h1>
+        {isGuestLogin ? (
+          <GuestLoginForm changeToUserMethod={() => setIsGuestLogin(false)} />
+        ) : (
+          <UserLoginForm changeToGuestMethod={() => setIsGuestLogin(true)} />
+        )}
+        <div className="auth-link__container">
+          Doesn't have an account?
+          <Link className="auth-link" to="/register">
+            Register
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
