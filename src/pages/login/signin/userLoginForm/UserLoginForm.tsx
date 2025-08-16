446c8ea807bdf766732d4e1df99f91f759d93bd2
yemishi <daviddperera10@gmail.com>
2025-08-16 12:47:04 +0100

    add user login form

---
+++ src/pages/login/signin/userLoginForm/UserLoginForm.tsx
@@ -0,0 +1,51 @@
+import { Input } from "@/components";
+import useForm, { type FormFields } from "@/hooks/useForm";
+import { authActions } from "@/lib/actions";
+
+type UserLoginFormProps = {
+  changeToGuestMethod?: () => void;
+};
+export default function UserLoginForm({ changeToGuestMethod }: UserLoginFormProps) {
+  const initialValues: FormFields = {
+    name: { value: "", min: 3, max: 15, minMessage: "Name must be at least 3 characters long." },
+    password: { value: "", min: 4, max: 20, minMessage: "Password must be at least 4 characters long." },
+  };
+  const { login } = authActions();
+  const { values, onChange, validateAll, errors } = useForm<{ name: string; password: string }>(initialValues);
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const isValid = validateAll();
+    if (!isValid) return;
+    login.mutate({ ...values });
+  };
+  return (
+    <form onSubmit={handleSubmit} className="auth-form">
+      <Input
+        error={errors.name || ""}
+        onChange={onChange}
+        required
+        name="name"
+        label="Name or Email"
+        placeholder="Who goes there?"
+      />
+      <Input
+        error={errors.password || ""}
+        onChange={onChange}
+        required
+        name="password"
+        label="Password"
+        placeholder="Your secret key"
+        isPassword
+      />
+      {changeToGuestMethod && (
+        <button className="auth-switch-method__button" type="button" onClick={changeToGuestMethod}>
+          Login as Guest
+        </button>
+      )}
+      {login.error?.message && <div className="auth-form__error">{login.error.message}</div>}
+      <button className="auth-submit__button" type="submit">
+        Sign In
+      </button>
+    </form>
+  );
+}
