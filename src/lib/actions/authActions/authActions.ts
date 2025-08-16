bcee3dac8252a536668b5aa657b7eb60f2a1cb11
yemishi <daviddperera10@gmail.com>
2025-08-16 12:42:58 +0100

    refactor auth actions and expand guest user flow

--- src/lib/actions/authActions/authActions.ts
+++ src/lib/actions/authActions/authActions.ts
@@ -1,5 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
-import { loginApi, registerApi, guestLoginApi, guestRegisterApi, signoutApi } from "@/lib/api";
+import {
+  loginApi,
+  registerApi,
+  guestLoginApi,
+  guestRegisterApi,
+  signoutApi,
+  type AuthGuestUser,
+  generateGuestApi,
+} from "@/lib/api";
 import { useAuth } from "@/context/AuthContext";
 import { connectSocket, disconnectSocket } from "@/lib/socket";
 import type { User } from "@/types";
@@ -9,38 +17,39 @@ export const authActions = () => {
   const { refetch, updateUser } = useAuth();
   const navigate = useNavigate();
   const search = useSearch({ strict: false });
-  const onSuccess = (data: { message: string; user: User }) => {
-    refetch();
+  const handleLogin = (data: { message: string; user: User }) => {
     connectSocket(data.user.id);
     updateUser(data.user);
     navigate({ to: search?.redirect || "/" });
   };
+
   const login = useMutation({
-    mutationFn: ({ email, password }: { email: string; password: string }) => loginApi(email, password),
-    onSuccess,
+    mutationFn: ({ name, password }: { name: string; password: string }) => loginApi(name, password),
+    onSuccess: handleLogin,
   });
 
   const register = useMutation({
-    mutationFn: ({
-      username,
-      email,
-      bio,
-      picture,
-    }: {
+    mutationFn: (values: {
       username: string;
       email: string;
+      password: string;
+      tag: string;
       bio?: string;
       picture?: string;
-    }) => registerApi(username, email, bio, picture),
-    onSuccess,
+    }) => registerApi(values),
+    onSuccess: handleLogin,
   });
   const guestRegister = useMutation({
-    mutationFn: () => guestRegisterApi(),
-    onSuccess,
+    mutationFn: (values: AuthGuestUser) => guestRegisterApi(values),
+    onSuccess: handleLogin,
+  });
+
+  const generateGuestUser = useMutation({
+    mutationFn: () => generateGuestApi(),
   });
   const guestLogin = useMutation({
-    mutationFn: ({ guestId }: { guestId: string }) => guestLoginApi(guestId),
-    onSuccess,
+    mutationFn: ({ guestCode }: { guestCode: string }) => guestLoginApi(guestCode),
+    onSuccess: handleLogin,
   });
   const signout = useMutation({
     mutationFn: signoutApi,
@@ -57,5 +66,6 @@ export const authActions = () => {
     register,
     guestLogin,
     guestRegister,
+    generateGuestUser,
   };
 };
