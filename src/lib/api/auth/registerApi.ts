c100b735e1f197b5e5e8ba77332e7079b69563ef
yemishi <daviddperera10@gmail.com>
2025-08-16 12:39:16 +0100

    create endpoint for generate user guest (generateGuestApi) and update user guestRegisterApi

--- src/lib/api/auth/registerApi.ts
+++ src/lib/api/auth/registerApi.ts
@@ -1,18 +1,42 @@
 import type { User } from "@/types";
 import { axiosInstance } from "../axiosInstance";
 
-export const guestRegisterApi = async () => {
+export type AuthGuestUser = {
+  id:string;
+  username: string;
+  picture: string;
+  guestCode: string;
+  tag: string;
+};
+
+export const guestRegisterApi = async (values: AuthGuestUser) => {
   try {
-    const response = await axiosInstance.post("/user/guest");
+    const response = await axiosInstance.post("/user/guest", values);
     return response.data as { user: User; message: string };
   } catch (error: any) {
     throw new Error(error.response.data.message);
   }
 };
 
-export const registerApi = async (username: string, email: string, bio?: string, picture?: string) => {
+export const generateGuestApi = async () => {
   try {
-    const response = await axiosInstance.post("/user", { username, email, bio, picture });
+    const response = await axiosInstance.get("/user/guest");
+    return response.data as { user: AuthGuestUser; message: string };
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const registerApi = async (values: {
+  username: string;
+  email: string;
+  tag: string;
+  password: string;
+  bio?: string;
+  picture?: string;
+}) => {
+  try {
+    const response = await axiosInstance.post("/user", { ...values });
     return response.data as { user: User; message: string };
   } catch (error: any) {
     throw new Error(error.response.data.message);
