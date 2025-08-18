913b8c172ec1a7692bce744dcaaf3647db7da803
yemishi <daviddperera10@gmail.com>
2025-08-18 20:46:27 +0100

    add UserSettings component

--- src/pages/account/Account.tsx
+++ src/pages/account/Account.tsx
@@ -1,6 +1,13 @@
-import { getSession } from "@/helpers";
+import UserSettings from "./userSettings/UserSettings";
+import "./styles.scss";
+import { useAuth } from "@/hooks";
 
 export default function Account() {
-  const user = getSession();
-  return <div>Account page</div>;
+  const { user } = useAuth()!;
+  return (
+    <div className="account-page">
+      <h1>Settings</h1>
+      <UserSettings user={user!} />
+    </div>
+  );
 }
