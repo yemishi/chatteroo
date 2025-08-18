b62bac93a53a726300a7fa8a6eba80e6d94b67fb
yemishi <daviddperera10@gmail.com>
2025-08-18 20:39:25 +0100

    add variant prop and BEM-compliant classes

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -12,25 +12,35 @@ interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
   required?: boolean;
   pattern?: string;
   isPassword?: boolean;
+  variant?: "primary" | "secondary";
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({ required, error, label, name, isPassword, pattern, ...props }: InputProps) {
-  const { id, type: typeProp = "text", ...rest } = props;
+export default function Input({
+  required,
+  error,
+  label,
+  name,
+  variant = "primary",
+  isPassword,
+  pattern,
+  ...props
+}: InputProps) {
+  const { id, type: typeProp = "text", className = "", ...rest } = props;
 
   const [isPasswordType, setIsPasswordType] = useState(isPassword);
   const type = isPasswordType ? "password" : typeProp;
   const inputId = id ?? `input-${label.toLowerCase().replace(/\s+/g, "-")}`;
   return (
-    <div className={`input-container ${error ? " input-container--error" : ""}`}>
-      <label className="input-label" htmlFor={inputId}>
+    <div className={`input-container input-container-${variant} ${className} ${error ? "input-container--error" : ""}`}>
+      <label className="input-container__label" htmlFor={inputId}>
         {label}
       </label>
-      <span className="input-wrapper">
+      <span className={`input-container__wrapper ${error ? "input-container__wrapper--error" : ""}`}>
         <input {...rest} pattern={pattern} name={name} required={required} type={type} id={inputId} />
         {isPassword && <EyeIcon open={!isPasswordType} onClick={() => setIsPasswordType(!isPasswordType)} />}
       </span>
-      {error && <span className="input-error">{error}</span>}
+      {error && <span className="input-container__error">{error}</span>}
     </div>
   );
 }
