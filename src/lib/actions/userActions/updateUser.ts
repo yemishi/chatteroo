993210a7d16fc1994a8594d44c2debd1d30dc334
yemishi <daviddperera10@gmail.com>
2025-08-18 20:49:05 +0100

    add user update actions file

---
+++ src/lib/actions/userActions/updateUser.ts
@@ -0,0 +1,31 @@
+import { useAuth } from "@/hooks";
+import { updateFromGuestApi, updateUserApi, type UpdateUserProps } from "@/lib/api";
+import type { User } from "@/types";
+import { useMutation } from "@tanstack/react-query";
+
+export function useUserUpdates() {
+  const { updateUser: authUpdateUser } = useAuth();
+  const onSuccess = (data: { user: User; message: string }) => {
+    authUpdateUser(data.user);
+  };
+  const updateUser = () => {
+    return useMutation({
+      mutationFn: (user: UpdateUserProps) => updateUserApi(user),
+      mutationKey: ["update-user"],
+      onSuccess,
+    });
+  };
+
+  const upgradeGuestAccount = () => {
+    return useMutation({
+      mutationFn: (user: { email: string; password: string }) => updateFromGuestApi(user),
+      mutationKey: ["update-user"],
+      onSuccess,
+    });
+  };
+
+  return {
+    updateUser,
+    upgradeGuestAccount,
+  };
+}
