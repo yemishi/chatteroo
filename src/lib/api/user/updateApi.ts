1fc90905087be6057fbcf38a13466b57e3b1dc1b
yemishi <daviddperera10@gmail.com>
2025-08-18 20:50:20 +0100

    add user update fetch file

---
+++ src/lib/api/user/updateApi.ts
@@ -0,0 +1,32 @@
+import type { User } from "@/types";
+import { axiosInstance } from "../axiosInstance";
+
+export const updateFromGuestApi = async (data: { email: string; password: string }) => {
+  try {
+    const response = await axiosInstance.patch("/user", data);
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export type UpdateUserProps = {
+  username?: string;
+  email?: string;
+  bio?: string;
+  tag?: string;
+  picture?: string;
+  password?: string;
+};
+
+export const updateUserApi = async (data: UpdateUserProps) => {
+  try {
+    const response = await axiosInstance.patch("/user", data);
+    return response.data as {
+      message: string;
+      user: User;
+    };
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
