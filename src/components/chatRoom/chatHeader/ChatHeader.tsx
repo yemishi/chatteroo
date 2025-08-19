de1ded125a508feee19a9cc4ae5d51709943e1a1
yemishi <daviddperera10@gmail.com>
2025-08-19 22:05:36 +0100

    apply BEM class naming for styling consistency

--- src/components/chatRoom/chatHeader/ChatHeader.tsx
+++ src/components/chatRoom/chatHeader/ChatHeader.tsx
@@ -13,13 +13,13 @@ export default function ChatHeader({ onClose, userHighlight: { picture, username
   const onlineUsers = getOnlineUsers();
   return (
     <header className="chat-header">
-      <button onClick={onClose}>
-        <ArrowLeft />
+      <button className="chat-header__close" onClick={onClose}>
+        <ArrowLeft className="chat-header__close__icon" />
       </button>
-      <div className="chat-user-info">
+      <div className="chat-header__info">
         <img
           src={picture}
-          className={onlineUsers.includes(id) ? "online" : "offline"}
+          className={`${onlineUsers.includes(id) ? "online" : "offline"} chat-header__info__icon`}
           alt={`${username}'s profile picture`}
         />
         <p>{username}</p>
