c4befc5a799bdb81109a542b05edd2c8e31a9667
yemishi <daviddperera10@gmail.com>
2025-08-21 19:58:36 +0100

    add ProfilePreview and logout button to Settings page

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -2,20 +2,29 @@ import { Outlet, useLocation } from "@tanstack/react-router";
 import "./styles.scss";
 import { useAuth } from "@/hooks";
 import SettingsCardGrid from "./settingsCardGrid/SettingsCardGrid";
-import { Header } from "@/components";
+import { Button } from "@/components";
+import ProfilePreview from "./profilePreview/ProfilePreview";
+import { useState } from "react";
+import DeleteAccount from "./deleteAccount/DeleteAccount";
 
 export default function Settings() {
-  const user = useAuth().user!;
+  const { logout, user } = useAuth();
   const match = useLocation().pathname === "/settings";
 
   return (
     <div className="settings-page">
       {match && (
         <>
-          <Header title="Settings" />
-          <SettingsCardGrid user={user} />
+          <ProfilePreview user={user!} />
+          <main>
+            <SettingsCardGrid user={user!} />
+          </main>
+          <Button variant="danger" className="settings-page__logout" onClick={logout}>
+            Log out
+          </Button>
         </>
       )}
+
       <Outlet />
     </div>
   );
