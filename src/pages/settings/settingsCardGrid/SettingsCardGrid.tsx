7e6e619e5b75394699e6102360da7fec8b9aaffd
yemishi <daviddperera10@gmail.com>
2025-08-21 19:45:11 +0100

    enhance SettingsCardGrid with conditional cards and delete account modal

--- src/pages/settings/settingsCardGrid/SettingsCardGrid.tsx
+++ src/pages/settings/settingsCardGrid/SettingsCardGrid.tsx
@@ -1,24 +1,54 @@
 import "./styles.scss";
 import type { User } from "@/types";
 import SettingsCard from "./settingsCard/SettingsCard";
+import { useState } from "react";
+import DeleteAccount from "../deleteAccount/DeleteAccount";
 
 type SettingsCardGridProps = {
   user: User;
 };
 export default function SettingsCardGrid({ user }: SettingsCardGridProps) {
+  const [isDeleteAcc, setIsDeleteAcc] = useState(false);
   return (
     <div className="settings-card-grid">
-      <SettingsCard
-        title="Edit Profile"
-        description="Change your profile picture, display name, and bio."
-        to="profile"
-      />
+      {user.guestCode && (
+        <SettingsCard
+          title="Upgrade account"
+          description="Upgrade from a guest account to unlock more features and customization."
+          to="upgrade_account"
+        />
+      )}
+      {!user.guestCode && (
+        <>
+          <SettingsCard
+            title="Edit Profile"
+            description="Change your profile picture, display name, and bio."
+            to="profile"
+          />
+          <SettingsCard
+            title="Update Tag"
+            description="Change your unique tag so friends can find you easily."
+            to="update_tag"
+          />
+          <SettingsCard
+            title="Update email"
+            description="Keep your account up to date by updating your email address."
+            to="update_email"
+          />
 
+          <SettingsCard
+            title="Update Secret"
+            description="Update your secret to keep your account secure."
+            to="update_secret"
+          />
+        </>
+      )}
       <SettingsCard
-        title="Upgrade account"
-        description="Upgrade from a guest account to unlock more features and customization."
-        to="upgrade__account"
+        title="Delete Account"
+        description="Update your secret to keep your account secure."
+        onClick={() => setIsDeleteAcc(true)}
       />
+      {isDeleteAcc && <DeleteAccount user={user} onClose={() => setIsDeleteAcc(false)} />}
     </div>
   );
 }
