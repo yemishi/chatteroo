d2dfa2bf35541608285bbc041e92f3a888deeffd
yemishi <daviddperera10@gmail.com>
2025-08-21 19:42:07 +0100

    add optional onClick and support for button-only card

--- src/pages/settings/settingsCardGrid/settingsCard/SettingsCard.tsx
+++ src/pages/settings/settingsCardGrid/settingsCard/SettingsCard.tsx
@@ -1,18 +1,36 @@
 import { Link } from "@tanstack/react-router";
 import "./styles.scss";
+import ArrowIcon from "@/assets/icons/arrow.svg?react";
 export default function SettingsCard({
   title,
   description,
   to,
+  onClick,
 }: {
   title: string;
   description: string;
-  to: "profile" | "upgrade__account";
+  to?: "profile" | "upgrade_account" | "update_tag" | "update_secret" | "update_email";
+  onClick?: () => void;
 }) {
+  if (to)
+    return (
+      <Link onClick={onClick} className="settings-card" to={`/settings/${to}`}>
+        <div className="settings-card__content">
+          <h3 className="settings-card__content-title">{title}</h3>
+          <p className="settings-card__content-description">{description}</p>
+        </div>
+        <button>
+          <ArrowIcon className="settings-card__icon" />
+        </button>
+      </Link>
+    );
+
   return (
-    <Link className="settings-card" to={`/settings/${to}`}>
-      <h3 className="settings-card__title">{title}</h3>
-      <p className="settings-card__label">{description}</p>
-    </Link>
+    <button onClick={onClick} className="settings-card">
+      <div className="settings-card__content">
+        <h3 className="settings-card__content-title">{title}</h3>
+        <p className="settings-card__content-description">{description}</p>
+      </div>
+    </button>
   );
 }
