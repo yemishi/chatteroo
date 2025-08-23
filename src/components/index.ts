402d65644d46145a4d216daff0f093c302f201bd
yemishi <daviddperera10@gmail.com>
2025-08-23 15:29:22 +0100

    exporting from ./toast/Toast

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./common/button";
 export * from "./common/input";
 export { default as Back } from "./goBack/Back";
 export { default as Header } from "./header/Header";
+export { default as Toast } from "./toast/Toast";
