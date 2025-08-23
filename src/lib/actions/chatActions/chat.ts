c1f566635554eecc851479d72a428c0505436cde
yemishi <daviddperera10@gmail.com>
2025-08-23 15:33:01 +0100

    add getChatInfoMutate functiong

--- src/lib/actions/chatActions/chat.ts
+++ src/lib/actions/chatActions/chat.ts
@@ -1,10 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getChatInfoApi, getChatsApi } from "../../api";
 
 export const getChatInfo = (chatId: string) => {
   return useQuery({
     queryFn: () => getChatInfoApi(chatId),
     queryKey: ["chat-info", chatId],
+  });
+};
+
+export const getChatInfoMutate = () => {
+  return useMutation({
+    mutationFn: (chatId: string) => getChatInfoApi(chatId),
+    mutationKey: ["chat-info-mutate"],
   });
 };
 
