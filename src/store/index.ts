aafb9515c87265de38fd890d8e035ddfbb484aff
yemishi <daviddperera10@gmail.com>
2025-08-23 15:36:44 +0100

    add toast reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import authReducer from "./authSlice";
+import authSlice from "./authSlice";
+import toastReducer from "./toastSlice";
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: authSlice,
+    toast: toastReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
