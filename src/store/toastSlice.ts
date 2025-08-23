fb31229f93f5103b9c51bc7056c10730991250d4
yemishi <daviddperera10@gmail.com>
2025-08-23 15:38:24 +0100

    add toastSlice

---
+++ src/store/toastSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+export type Toast = {
+  id: string;
+  title: string;
+  icon?: string;
+  message?: string;
+  type: "success" | "error" | "info" | "message";
+  duration?: number;
+  onClick?: () => void;
+};
+
+type ToastsState = {
+  list: Toast[];
+};
+
+const initialState: ToastsState = {
+  list: [],
+};
+
+export const toastsSlice = createSlice({
+  name: "toast",
+  initialState,
+  reducers: {
+    addToast(state, action: PayloadAction<Toast>) {
+      state.list.push(action.payload);
+    },
+    removeToast(state, action: PayloadAction<string>) {
+      state.list = state.list.filter((n) => n.id !== action.payload);
+    },
+  },
+});
+
+export const { addToast, removeToast } = toastsSlice.actions;
+export default toastsSlice.reducer;
