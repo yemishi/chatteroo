c0932fdb8358523e3590bf7a402393c15bdab19b
yemishi <daviddperera10@gmail.com>
2025-08-25 12:20:13 +0100

    add highlight styles for unread chat previews

--- src/components/chatList/ChatList.tsx
+++ src/components/chatList/ChatList.tsx
@@ -5,12 +5,14 @@ import { useEffect, useState, type Dispatch, type SetStateAction } from "react";
 import type { Chat as ChatType } from "@/types";
 import { isSameDay, isYesterday } from "@/helpers";
 import getOnlineUsers from "@/lib/getOnlineUsers";
+import { useAuth } from "@/hooks";
 
 type Props = {
   setChat: Dispatch<SetStateAction<ChatType | null>>;
   searchChat: string;
 };
 export default function ChatList({ setChat, searchChat }: Props) {
+  const currUser = useAuth().user!;
   type Message = { chatId: string; content: string; senderId: string; timestamp: Date };
   const { data } = getChats();
 
@@ -39,6 +41,7 @@ export default function ChatList({ setChat, searchChat }: Props) {
       socket.off("chat-updated", handleUpdate);
     };
   }, [data]);
+
   const onlineUsers = getOnlineUsers();
   const getDataLastMsg = (i: number) => data && data[i] && (data[i].messages.length ? data[i].messages[0] : null);
   const sortedChats = (data ?? [])
@@ -52,15 +55,27 @@ export default function ChatList({ setChat, searchChat }: Props) {
       const bTime = b.messages[0]?.timestamp ?? 0;
       return new Date(bTime).getTime() - new Date(aTime).getTime();
     });
+
+  const isRead = (date?: Date, uLastTimeRead?: Date) => new Date(uLastTimeRead || 0) > new Date(date || 0);
   return (
     <div className="chat-list">
       {sortedChats.map((c, i) => {
         const user = c.highlight;
+        const lastTimeRead = c.members.find((m) => m.id === currUser.id)?.lastMessageReadAt;
         const newMsg = newMsgs.find((msg) => msg.chatId === c.id);
         const isOnline = onlineUsers.some((i) => i === user?.id);
         const messageTime = new Date(newMsg?.timestamp || getDataLastMsg(i)?.timestamp!);
+        const latestMessage = c.messages.length > 0 || newMsg ? newMsg || c.messages[0] : null;
         return (
-          <div key={c.id} className="chat-preview" onClick={() => setChat(c)}>
+          <div
+            key={c.id}
+            className={`chat-preview ${
+              isRead(latestMessage?.timestamp, lastTimeRead) || latestMessage?.senderId === currUser.id
+                ? ""
+                : "chat-preview--unread"
+            }`}
+            onClick={() => setChat(c)}
+          >
             <div className="chat-preview__icon">
               <img src={user.picture} alt={`${user.username} picture`} />
               <span className={`chat-preview__presence ${isOnline ? "online" : "offline"}`} />
@@ -80,12 +95,11 @@ export default function ChatList({ setChat, searchChat }: Props) {
                 )}
               </div>
 
-              <div className="chat-preview__content">
-                {(c.messages.length > 0 || newMsg) && (
-                  <p className="chat-preview__message">{newMsg?.content || c.messages[0].content}</p>
-                )}
-                <div className="chat-preview__unread">{/* todo implement unread message flow */}</div>
-              </div>
+              {latestMessage && (
+                <div className="chat-preview__content">
+                  <p className="chat-preview__message">{latestMessage.content}</p>
+                </div>
+              )}
             </div>
           </div>
         );
