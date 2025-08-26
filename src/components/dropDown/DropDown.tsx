89138dac5d71882d6a7b198ed74ed3c1fe4ede58
yemishi <daviddperera10@gmail.com>
2025-08-26 19:27:31 +0100

    add useOverlay hook to handle on closing

--- src/components/dropDown/DropDown.tsx
+++ src/components/dropDown/DropDown.tsx
@@ -1,38 +1,18 @@
-import "./style.scss";
+import { useOverlay } from "@/hooks";
+import "./styles.scss";
 
-import { useEffect, useRef, type HTMLAttributes, type MouseEvent, type ReactNode } from "react";
+import { useRef, type HTMLAttributes, type ReactNode, type RefObject } from "react";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
   onClose: () => void;
   isOpen: boolean;
+  containerRef?: RefObject<HTMLDivElement | null>;
 }
-export default function DropDown({ onClose, isOpen, className = "", children }: Props) {
+export default function DropDown({ onClose, isOpen, className = "", children, containerRef }: Props) {
   const dropdownRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (isOpen) {
-      const originalStyle = window.getComputedStyle(document.body).overflow;
-      document.body.style.overflow = "hidden";
-      return () => {
-        document.body.style.overflow = originalStyle;
-      };
-    }
-  }, [isOpen]);
-
-  useEffect(() => {
-    if (!isOpen) return;
-
-    const handleClickOutside = (e: MouseEvent | globalThis.MouseEvent) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
-        onClose();
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isOpen]);
+  const extraRef = (containerRef ?? null) as RefObject<HTMLDivElement | null>;
+  useOverlay(isOpen, onClose, [dropdownRef, extraRef]);
 
   return (
     <div ref={dropdownRef} className={`${className} dropdown ${isOpen ? "open" : ""}`}>
