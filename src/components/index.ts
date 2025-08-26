3d41184f2fc3be4e0d8ada8ddcbba191c706a02c
yemishi <daviddperera10@gmail.com>
2025-08-26 17:14:50 +0100

    exporting from ./popover/Popover n ./confirmModal/ConfirmModal

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export * from "./common/input";
 export { default as Back } from "./goBack/Back";
 export { default as Header } from "./header/Header";
 export { default as Toast } from "./toast/Toast";
+export { default as Popover } from "./popover/Popover";
+export { default as ConfirmModal } from "./confirmModal/ConfirmModal";
