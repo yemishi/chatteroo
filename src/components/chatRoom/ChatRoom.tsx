0a4ae73f697596feef114f459195458d8fe1f6a8
yemishi <daviddperera10@gmail.com>
2025-08-26 08:14:08 +0100

    add quit chat confirmation modal and mutation

--- src/components/chatRoom/ChatRoom.tsx
+++ src/components/chatRoom/ChatRoom.tsx
@@ -8,6 +8,8 @@ import Modal from "../modal/Modal";
 import Messages from "./messages/Messages";
 import ChatHeader from "./chatHeader/ChatHeader";
 import { useAuth, useChatRoom } from "@/hooks";
+import ConfirmModal from "../confirmModal/ConfirmModal";
+import { quitChat } from "@/lib/actions";
 
 type Props = {
   chatInfo: Chat;
@@ -37,7 +39,8 @@ export default function ChatRoom({ chatInfo, onClose, scrollPositions, setScroll
   });
   const [isOpen, setIsOpen] = useState(false);
   const [message, setMessage] = useState("");
-
+  const [isQuitChat, setIsQuitChat] = useState(false);
+  const { mutateAsync, status } = quitChat();
   useEffect(() => {
     setIsOpen(true);
   }, []);
@@ -56,42 +59,70 @@ export default function ChatRoom({ chatInfo, onClose, scrollPositions, setScroll
     setMessage("");
   };
 
+  const onQuitChat = async () => {
+    try {
+      await mutateAsync(chatInfo.id);
+      setIsQuitChat(false);
+      handleClose();
+    } catch (error) {
+      console.log(error);
+      console.error(error);
+    }
+  };
   return (
-    <Modal
-      ref={containerRef as React.RefObject<HTMLDivElement>}
-      onClose={handleClose}
-      isOpen={isOpen}
-      className="chat-modal"
-    >
-      <ChatHeader onClose={handleClose} userHighlight={chatInfo.highlight} />
-
-      {isFetchingNextPage && <div>Loading older messages</div>}
-      <Messages messages={values} members={chatInfo.members} currMember={currMember!} highLight={chatInfo.highlight} />
-      <div className="chat-room__type-area">
-        <button
-          onClick={scrollToBottom}
-          className={`chat-room__scroll-action ${hasUnreadMessage ? "badged" : ""} ${
-            showScrollButton ? "show" : "hide"
-          }`}
-        >
-          <ArrowLeft className="chat-room__scroll-action__icon" />
-        </button>
-
-        <input
-          type="text"
-          name="message"
-          className="primary-input"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Type a message..."
-          onKeyDown={(e) => {
-            if (e.key === "Enter") sendMessage();
-          }}
+    <>
+      <Modal
+        ref={containerRef as React.RefObject<HTMLDivElement>}
+        onClose={handleClose}
+        isOpen={isOpen}
+        className="chat-modal"
+      >
+        <ChatHeader
+          toggleQuitChat={() => setIsQuitChat(!isQuitChat)}
+          onClose={handleClose}
+          userHighlight={chatInfo.highlight}
         />
-        <button onClick={sendMessage}>
-          <SendIcon />
-        </button>
-      </div>
-    </Modal>
+
+        {isFetchingNextPage && <div>Loading older messages</div>}
+        <Messages
+          messages={values}
+          members={chatInfo.members}
+          currMember={currMember!}
+          highLight={chatInfo.highlight}
+        />
+        <div className="chat-room__type-area">
+          <button
+            onClick={scrollToBottom}
+            className={`chat-room__scroll-action btn ${hasUnreadMessage ? "badged" : ""} ${
+              showScrollButton ? "show" : "hide"
+            }`}
+          >
+            <ArrowLeft className="chat-room__scroll-action__icon" />
+          </button>
+
+          <input
+            type="text"
+            name="message"
+            className="primary-input"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Type a message..."
+            onKeyDown={(e) => {
+              if (e.key === "Enter") sendMessage();
+            }}
+          />
+          <button className="btn" onClick={sendMessage}>
+            <SendIcon />
+          </button>
+        </div>
+      </Modal>
+      <ConfirmModal
+        warnMsg="Leaving this chat will also remove the participants from your friends list."
+        isLoading={status === "pending"}
+        onConfirm={onQuitChat}
+        isOpen={isQuitChat}
+        onClose={() => setIsQuitChat(false)}
+      />
+    </>
   );
 }
