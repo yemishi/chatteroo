eaa73fd04cf47d5b6406b76475f7b21fade2cb62
yemishi <daviddperera10@gmail.com>
2025-09-03 12:38:03 +0100

    exporting from ./carousel/Carousel

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as Header } from "./header/Header";
 export { default as Toast } from "./toast/Toast";
 export { default as Popover } from "./popover/Popover";
 export { default as ConfirmModal } from "./confirmModal/ConfirmModal";
+export { default as Carousel } from "./carousel/Carousel";
