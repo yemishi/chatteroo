0b64505a31f77b2c688186f90cb2ac833a848fc8
yemishi <daviddperera10@gmail.com>
2025-09-03 12:36:40 +0100

    add variant prop to change label style

--- src/components/common/input/inputFile/InputFile.tsx
+++ src/components/common/input/inputFile/InputFile.tsx
@@ -3,10 +3,11 @@ import ImageIcon from "@/assets/icons/picture.svg?react";
 
 interface Props extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange"> {
   size?: number;
+  variant?: "overlay";
   onChange?: (data: { preview: string; file: File }) => void;
 }
 
-export default function InputFile({ size = 24, ...props }: Props) {
+export default function InputFile({ size = 24, variant, ...props }: Props) {
   const { className = "", onChange, ...rest } = props;
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0] || null;
@@ -19,8 +20,9 @@ export default function InputFile({ size = 24, ...props }: Props) {
 
     onChange?.(data);
   };
+
   return (
-    <label className="input-file">
+    <label className={`input-file ${variant ? `input-file__variant-${variant}` : ""}`}>
       <ImageIcon className="input-file--placeholder" width={size} height={size} />
       <input {...rest} onChange={handleFileChange} type="file" />
     </label>
