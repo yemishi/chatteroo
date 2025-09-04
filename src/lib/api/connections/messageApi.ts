495d63f00aee73042a1443278a940a8a30dd7dee
yemishi <daviddperera10@gmail.com>
2025-09-04 21:37:41 +0100

    add editMessagePai and deleteMessageApi actions

--- src/lib/api/connections/messageApi.ts
+++ src/lib/api/connections/messageApi.ts
@@ -1,8 +1,21 @@
 import { axiosInstance } from "../axiosInstance";
 
-export const editMessageApi = async (msgId: string, content: string) => {
+export const editMessageApi = async (
+  msgId: string,
+  content: { text?: string; imgs: string[] },
+  removedImgs: string[]
+) => {
   try {
-    const response = await axiosInstance.patch(`/message?messageId=${msgId}`, content);
+    const response = await axiosInstance.patch("/message", { content, removedImgs, msgId });
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const deleteMessageApi = async (msgId: string, imgs: string[]) => {
+  try {
+    const response = await axiosInstance.delete("/message", { data: { msgId, imgs } });
     return response.data;
   } catch (error: any) {
     throw new Error(error.response.data.message);
