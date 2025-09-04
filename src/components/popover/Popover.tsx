bf12c27ef6bc97f0e3d83cc9f5e2fd39dc658154
yemishi <daviddperera10@gmail.com>
2025-09-04 21:34:22 +0100

    add autoClose option to Popover and update useOverlay usage

--- src/components/popover/Popover.tsx
+++ src/components/popover/Popover.tsx
@@ -7,11 +7,12 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
   onClose: () => void;
   isOpen: boolean;
+  autoClose?: boolean;
   variant?: "primary" | "secondary";
 }
-export default function Popover({ onClose, isOpen, className = "", children, variant = "primary" }: Props) {
+export default function Popover({ onClose, isOpen, className = "", children, autoClose, variant = "primary" }: Props) {
   const dropdownRef = useRef<HTMLDivElement>(null);
-  useOverlay(isOpen, onClose, [dropdownRef]);
+  useOverlay({ isOpen, onClose, refs: [dropdownRef], autoClose });
 
   return (
     <div ref={dropdownRef} className={`${className} popover popover-${variant} ${isOpen ? "open" : ""}`}>
