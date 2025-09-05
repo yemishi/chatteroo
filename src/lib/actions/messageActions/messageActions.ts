e10fa00ea692979ed02003bae04aa9e891d4ea71
yemishi <daviddperera10@gmail.com>
2025-09-05 11:47:20 +0100

    add deleteMessage mutation and improve editMessage with cache invalidation

--- src/lib/actions/messageActions/messageActions.ts
+++ src/lib/actions/messageActions/messageActions.ts
@@ -1,15 +1,38 @@
-import { useMutation } from "@tanstack/react-query";
-import { editMessageApi } from "../../api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { editMessageApi, deleteMessageApi } from "../../api";
 import useScrollQuery from "@/hooks/useScrollQuery";
 import type { Message } from "@/types";
 
 export const getMessages = (chatId: string) => {
-  return useScrollQuery<Message>({ url: `/message/${chatId}`, queryKey: [chatId] });
+  return useScrollQuery<Message>({ url: `/message/${chatId}`, queryKey: [`get-message-${chatId}`] });
 };
 
-export const editMessage = () => {
+export const editMessage = (chatId: string) => {
+  const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: ({ msgId, content }: { msgId: string; content: string }) => editMessageApi(msgId, content),
+    mutationFn: ({
+      msgId,
+      content,
+      removedImgs,
+    }: {
+      msgId: string;
+      content: { text?: string; imgs: string[] };
+      removedImgs?: string[];
+    }) => editMessageApi(msgId, content, removedImgs || []),
     mutationKey: ["edit-message"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [`get-message-${chatId}`] });
+    },
+  });
+};
+
+export const deleteMessage = (chatId: string) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ msgId, imgs }: { msgId: string; imgs: string[] }) => deleteMessageApi(msgId, imgs),
+    mutationKey: ["edit-message"],
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [`get-message-${chatId}`] });
+    },
   });
 };
