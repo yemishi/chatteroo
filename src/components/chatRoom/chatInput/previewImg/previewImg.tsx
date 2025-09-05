12c0f4e5a66c4656b8a48508b448f19173386af2
yemishi <daviddperera10@gmail.com>
2025-09-05 11:44:22 +0100

    update removeImg prop adding optional url parameter

--- src/components/chatRoom/chatInput/previewImg/previewImg.tsx
+++ src/components/chatRoom/chatInput/previewImg/previewImg.tsx
@@ -3,7 +3,7 @@ import RemoveIcon from "@/assets/icons/close.svg?react";
 import Carousel from "@/components/carousel/Carousel";
 type Props = {
   imgs: string[];
-  removeImg: (i: number) => void;
+  removeImg: (i: number, url?: string) => void;
 };
 export default function PreviewImg({ imgs, removeImg }: Props) {
   if (!imgs.length) return null;
@@ -13,7 +13,7 @@ export default function PreviewImg({ imgs, removeImg }: Props) {
       {imgs.map((url, i) => (
         <div key={i} className="preview-img__item">
           <img src={url} alt="image preview" className="preview-img__pic" />
-          <button onClick={() => removeImg(i)} className="preview-img__remove-btn">
+          <button onClick={() => removeImg(i, url)} className="preview-img__remove-btn">
             <RemoveIcon />
           </button>
         </div>
